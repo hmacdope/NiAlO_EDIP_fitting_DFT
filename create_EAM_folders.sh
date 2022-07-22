2cdcd0dbd227973dc05d5fa47df4b11340f26edb
hmacdope <hugomacdermott@gmail.com>
2022-07-22 16:04:57 +1000

    do up to NiAl

---
+++ create_EAM_folders.sh
@@ -0,0 +1,15 @@
+for d in */ ; do
+    echo "$d looking"
+    cd $d
+    if [ ! -d "./EAM_energy" ]
+    then
+	    mkdir EAM_energy
+	    cd "./EAM_energy"
+	    echo "made EAM folder"
+	    pwd
+	    cp ../*.data .
+	    cp /store/Terry_project/EAM_energy_base/* .
+	    cd ../
+    fi
+    cd ../
+done
--- run-all-lammps.sh
+++ run-all-lammps.sh
@@ -1,3 +1,7 @@
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+GREEN='\033[0;32m'
+
 for d in */ ; do
     echo "$d looking"
     cd $d
@@ -7,6 +11,12 @@ for d in */ ; do
 	    echo "doing EAM for"
 	    pwd
 	    lmp -in single_point.run > single_point.out
+	    if [ $? -ne 0 ]
+	    then
+		    echo -e "${RED}FAILED${NC}"
+	    else
+		    echo -e "${GREEN}DONE${NC}"
+	    fi
 	    cd ../
     fi
     cd ../
